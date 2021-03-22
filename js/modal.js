5679d49731b16106bd9e0c5876215426cf71910d
eeliseev31 <77843563+eeliseev31@users.noreply.github.com>
2021-03-22 23:47:43 +0300

    Сделал сырое открытие/закрытие формы

---
+++ js/modal.js
@@ -0,0 +1,16 @@
+const modalButton = document.querySelector(".hotel-search");
+const hideForm = document.querySelector(".hide-form");
+const arriveForm = document.querySelector(".text");
+const modalForm = document.querySelector(".form");
+const outForm = document.querySelector(".text-out");
+
+modalButton.addEventListener("click", function () {
+  hideForm.classList.toggle("hide-form");
+  arriveForm.focus();
+});
+
+modalForm.addEventListener("submit", function (evt) {
+    if (!arriveForm.value || !outForm.value)
+    evt.preventDefault()
+});
+
