65b9d01d57bb274fb3f56ce6e72804c4d00198d5
eeliseev31 <77843563+eeliseev31@users.noreply.github.com>
2021-03-24 21:27:03 +0300

    add minor changes

--- js/modal.js
+++ js/modal.js
@@ -1,13 +1,15 @@
 const modalButton = document.querySelector(".hotel-search");
 const hideForm = document.querySelector(".hide-form");
 const arriveForm = document.querySelector(".text");
-const modalForm = document.querySelector(".form");
+const modalForm = document.querySelector(".search-wrapper");
 const outForm = document.querySelector(".text-out");
 
 modalButton.addEventListener("click", function () {
   hideForm.classList.toggle("hide-form");
   arriveForm.focus();
 });
+
+
 
 modalForm.addEventListener("submit", function (evt) {
     if (!arriveForm.value || !outForm.value)
